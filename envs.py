b7e7f4ec9fdb9c7a26c72aebede02968d592bfa5
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2019-05-05 16:38:58 -0400

    mountain car env shape

--- envs.py
+++ envs.py
@@ -7,8 +7,12 @@ from gym.spaces.box import Box
 # Taken from https://github.com/openai/universe-starter-agent
 def create_atari_env(env_id):
     env = gym.make(env_id)
-    env = AtariRescale42x42(env)
-    env = NormalizedEnv(env)
+
+    if env_id == 'MountainCar-v0':
+        env = MountainCarDiscrete(env)
+    else:
+        env = AtariRescale42x42(env)
+        env = NormalizedEnv(env)
     return env
 
 
@@ -26,12 +30,36 @@ def _process_frame42(frame):
     return frame
 
 
+class MountainCarDiscrete(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(MountainCarDiscrete, self).__init__(env)
+        self.observation_space_orig = self.observation_space
+        self.bins = [np.linspace(self.observation_space.low[i],
+                                 self.observation_space.high[i],
+                                 32) for i in [0,1]]
+        self.observation_space = Box(0.0, 1.0, [1, 32,32])
+    
+    def observation(self, observation):
+        """
+            Return discretized observation.
+            32x32 grid with a 1 at the observation.
+        """
+        coord = []
+        for obs, bins in zip(observation, self.bins):
+            ind = np.digitize(obs, bins)
+            coord.append(ind)
+
+        x = np.zeros((32, 32))
+        x[coord] = 1.0
+        # x = x.flatten()
+        return np.array([x])
+
 class AtariRescale42x42(gym.ObservationWrapper):
     def __init__(self, env=None):
         super(AtariRescale42x42, self).__init__(env)
         self.observation_space = Box(0.0, 1.0, [1, 42, 42])
 
-    def _observation(self, observation):
+    def observation(self, observation):
         return _process_frame42(observation)
 
 
@@ -43,7 +71,7 @@ class NormalizedEnv(gym.ObservationWrapper):
         self.alpha = 0.9999
         self.num_steps = 0
 
-    def _observation(self, observation):
+    def observation(self, observation):
         self.num_steps += 1
         self.state_mean = self.state_mean * self.alpha + \
             observation.mean() * (1 - self.alpha)
@@ -53,4 +81,7 @@ class NormalizedEnv(gym.ObservationWrapper):
         unbiased_mean = self.state_mean / (1 - pow(self.alpha, self.num_steps))
         unbiased_std = self.state_std / (1 - pow(self.alpha, self.num_steps))
 
-        return (observation - unbiased_mean) / (unbiased_std + 1e-8)
+        obs = (observation - unbiased_mean) / (unbiased_std + 1e-8)
+        print(obs)
+        print(obs.shape)
+        return obs
